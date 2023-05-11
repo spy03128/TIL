95f61bb81fe3da783e041ec28b0792bff268259b
spy03128 <spy03128@naver.com>
2023-05-11 19:05:04 +0900

    [이코테]1이 될 때까지

---
+++ python/이것이코딩테스트다/그리디/1이 될 때까지.py
@@ -0,0 +1,16 @@
+n, k = map(int,input().split())
+res = 0
+
+while n>=k:
+    while n % k != 0:
+        n -=1
+        res +=1
+
+    n //= k
+    res+=1
+
+while n>1:
+    n-=1
+    res+=1
+
+print(res)
